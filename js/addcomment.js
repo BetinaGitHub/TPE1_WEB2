b7e3dea26ab61b66503468235af449123648ebe4
Josefina Lopez Vivas <mjlvivas@gmail.com>
2020-11-28 19:37:22 -0300

    Arreglo problema con archivo addcomment

--- js/addcomment.js
+++ js/addcomment.js
@@ -1,32 +1,82 @@
-'use strict';
+"use strict";
+
+/* const app = new Vue({
+    el: "#app",
+    data: {
+        comments: [], // esto es como un assign de smarty
+    },
+ 
+
+}); */
+
 
 document.addEventListener('DOMContentLoaded', e => {
-    let idmaq = document.querySelector('param[name=idmaq]').value;
-    getComments(idmaq);
-    document.querySelector('#comment-form').addEventListener('submit', e => {
+    //  getCommentsbyIdTool();
+    //   e.preventDefault();
+
+    let form_alta_comment = document.querySelector("#form_alta_comment");
+    let score = document.querySelector("input[name=r_button_score]:checked");
+    //   document.querySelector('input[name=nameOfradio]:checked').value;
+    let det_comment = document.querySelector("#det_comment");
+    const idmaq = document.querySelector("#idtool").value;
+
+    getCommentsbyIdTool();
+    let iduser = 1;
+    console.log(idmaq);
+
+
+
+    //e.preventDefault();
+    form_alta_comment.addEventListener("submit", function(e) {
+        console.log('Entro al alta');
+        let score = document.querySelector("input[name=r_button_score]:checked").value;
         e.preventDefault();
-        addComment(idmaq);
+        const comentario = {
+            "comentario": det_comment.value,
+            "puntaje": score,
+            "idmaq": idmaq,
+            "iduser": iduser
+        }
+        console.log(comentario);
+        addComment(comentario);
+        getCommentsbyIdTool();
     });
-});
 
-//Inserta un comentario a una maquina
-async function addComment(idMaq) {
-    const comment = {
-        user: document.querySelector('param[name=username]').value,
-        id_user: document.querySelector('param[name=user]').value,
-        comment: document.querySelector('textarea[name=comment]').value,
-        puntaje: document.querySelector('#valoracion').innerHTML,
-    }
-    try {
-        const response = await fetch('../api/comments/' + idmaq, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(comment)
-        });
+    async function addComment(comentario) {
 
-        const c = await response.json();
-        document.querySelector('textarea[name=comment]').value = '';
-        getComments(idSkin);
-    } catch (e) {
-        console.log(e);
+        try {
+            const response = await fetch('../api/comentarios', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(comentario)
+            });
+            console.log(response);
+            const datos = await response.json();
+            console.log(datos);
+            app.tareas.push(datos);
+
+
+        } catch (error) {
+            console.log(error);
+        }
+        //    e.preventDefault();
     }
+
+    async function getCommentsbyIdTool() {
+        //   const urlapi = 'api/comentarios/';
+        console.log('aca entra');
+        //console.log(idmaq);
+        try {
+            const response = await fetch('../api/comentarios/' + idmaq);
+            //        console.log(response);
+            const datos = await response.json();
+            //        console.log(datos);
+            // imprimo los comentarios
+            app.comments = datos;
+
+        } catch (e) {
+            console.log(error);
+        }
+
+    }
+})
